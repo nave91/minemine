91e7a6e2a1a883454d012c4ce20fa49f59566f67
nave91 <nave30041991@gmail.com>
2013-09-15 23:47:30 -0400

    completed proj1d

--- src/readered.py
+++ src/readered.py
@@ -3,7 +3,7 @@ from table import *
 from sys import argv
 from xval import *
 
-csvfile = open('../data/diabetes.csv','r')
+csvfile = open('../data/iris.csv','r')
 readCsv(csvfile,argv[1]) #takes predicted value as arguement
 xvals(data,5,5,'zeror',argv[1],1,2)
                                  
