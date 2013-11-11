c360415f5d991e2223bb5126dd3d6958706f9dbe
nave91 <nave30041991@gmail.com>
2013-11-11 16:21:32 -0500

    adding projidea

--- out/outit.sh
+++ out/outit.sh
@@ -1,5 +1,5 @@
 Proj=$1
 Title="573:"$Proj":Naveen Kumar Lekkalapudi"
 echo $Title
-a2ps --center-title="$Title" -qr2gC -o ~/git/minemine/out/$Proj.ps $2 $3#~/git/minemine/src/*
+a2ps --center-title="$Title" -qr2gC -o ~/git/minemine/out/$Proj.ps ~/git/minemine/src/*
 ps2pdf ~/git/minemine/out/$Proj.ps
--- src/rank.py
+++ src/rank.py
@@ -15,14 +15,17 @@ def obs(f,alli):
                 v = float(i)
                 inc(v,now)
                 inc(v,alli)
-        f.close()
         for i in name:
             if i != alli:
-                order[i]["="] = i
-                order[i]["x"] = mu[i]
+                temp = {}
+                temp["="] = i
+                temp["x"] = mu[i]
+                order.append(temp)
         line = f.readline()
 
 def inc(v,k):
+    print v,"vvvvvvvvvvvvvvvvvvv"
+    print k,"kkkkkkkkkkkkk"
     name.append(k)
     label[k] = 0
     try:
@@ -39,43 +42,51 @@ def inc(v,k):
         sumi[k] += v
     except KeyError:
         sumi[k] = v
-    delta = v - mu[k]
+    try:
+        delta = v - mu[k]
+    except KeyError:
+        mu[k] = 0
+        delta = v - mu[k]
     try:
         mu[k] += delta/alli
     except KeyError:
-        print "hello"
         mu[k] = delta/alli
-    print k
-    print mu[k]
     try:
         m2[k] += delta*(v - mu[k])
     except KeyError:
         m2[k] = delta*(v - mu[k])
     var[k] = m2[k]/(alli - 1 + PINCH)
 
-def rank(alli,a,cohen,mittas,a12):
+def rank(alli,cohen,mittas,a12):
     cohen = cohen*(var[alli])**0.5
     level = 0 
-    total = ns[alli]
-    rdiv(0,len(order),1,cohen,mittas,a12)
+    total = n[alli]
+    rdiv(0,len(order)-1,1,cohen,mittas,a12,level)
     
-def rdiv(low,high,c,cohen,mittas,a12):
+def rdiv(low,high,c,cohen,mittas,a12,level):
     cut = div(low,high,cohen,mittas,a12)
     if cut:
+        print "in cut",cut
         level += 1
-        c = rdiv(low,cut-1,c) + 1
-        c = rdiv(cut,high,c)
+        c = rdiv(low,cut-1,c,cohen,mittas,a12,level) + 1
+        c = rdiv(cut,high,c,cohen,mittas,a12,level)
     else:
         for i in range(low,high):
-            labels[order[i]["="]] = c
+            print order[i]["="],"orderrrrrrrrrr",c
+            label[order[i]["="]] = c
     return c
 
-def div(low,high):
-    muAll = divInits(low,high)
+def div(low,high,cohen,mittas,a12):
+    n0 = [0 for i in range(0,len(order))]
+    n1 = [0 for i in range(0,len(order))]
+    sum0 = [0 for i in range(0,len(order))]
+    sum1 = [0 for i in range(0,len(order))]
+    muAll = divInits(low,high,n0,n1,sum0,sum1)
     maxi = -1
+    cut = 0
     for i in range(low,high):
         b = order[i]["="]
-        n0[i] = n0[i-1] + ns[b]
+        n0[i] = n0[i-1] + n[b]
         sum0[i] = sum0[i-1] + sumi[b]
         left = n0[i]
         muLeft = sum0[i] / left
@@ -93,57 +104,67 @@ def div(low,high):
                 continue
         maxi = e
         cut = i
+    print cut,"cutttt"
     return cut
 
 def errDiff(mu,n0,mu0,n1,mu1):
     return n0*(mu - mu0)**2 + n1*(mu - mu1)**2
 
-def diffInits(low,high):
-    b= order[lo]["="]
-    n0[lo]= n[b]
-    sum0[lo]= sumi[b]
-    b= order[hi]["="]
-    n1[hi]= n[b]
-    sum1[hi]= sumi[b]
-    for i in range(hi,lo-1,-1):
+def divInits(low,high,n0,n1,sum0,sum1):
+    b= order[low]["="]
+    n0[low]= n[b]
+    sum0[low]= sumi[b]
+    b= order[high]["="]
+    n1[high]= n[b]
+    sum1[high]= sumi[b]
+    for i in range(high-1,low-1,-1):
         b = order[i]["="]
         n1[i] = n1[i+1] + n[b]
         sum1[i] = sum1[i+1] + sumi[b]
-    return sum1[low]/n1[low]
+    return sum1[low+1]/n1[low+1]
 
 def bigger(low,mid,high):
     below =  []
     above = []
-    values(below,low,mid-1)
-    values(above,mid,high)
+    below = values(low,mid-1)
+    above = values(mid,high)
     return a12statistic(below,above)
 
 def a12statistic(below,above):
-    for j in enumerate(above):
-        for i in enumerate(below):
+    more = 0
+    same = 0
+    comparisons = 1
+    for j in range(0,len(above)-1):
+        for i in range(0,len(below)-1):
             comparisons += 1
-            more += above[j] if above[j] > below[j] else below[i]
-            same += above[j] if above[j] == below[j] else below[i]
+            more += above[j] if above[j] > below[i] else below[i]
+            same += above[j] if above[j] == below[i] else below[i]
     return (more + 0.5*same)/comparisons
 
-def values(out,i,j):
+def values(i,j):
     out = []
-    m = 0
+    m = 0    
     for k in range(i,j):
         b = order[k]["="]
-        for l in enumerate(x[b]):
+        for l,n in enumerate(x[b]):
             m += 1
-            out[m] = x[b][l]
-    return m
+            out.append(x[b][n])
+    return out
 
 def ranks(f,cohens,mittas,a12):
     print "\n----|,",f.name,"|------------------"
     obs(f,0)
-    rank(0,a,cohens,mittas,a12)
+    rank(0,cohens,mittas,a12)
     maxi = len(order)
     for i in range(0,maxi):
         k = order[i]["="]
-        print k,names[k],":mu",mu[k],":rank",labels[k]        
+        print k
+        print name,"nameee"
+        print mu,"muuu"
+        print label,"rank"
+    
+        print k,name[k],":mu",mu[k],":rank",label[k]        
 
 f = open('../data/ska.txt','r')
 ranks(f,0.3,1,0.6)
+f.close()
